cdae4174e8714c05ce55986ff318e593562fcab7
christian.benseler <cbenseler@gmail.com>
2020-10-26 13:43:01 -0300

    test: authorization create auth

--- authorization/services/authorization_test.go
+++ authorization/services/authorization_test.go
@@ -2,6 +2,9 @@ package services
 
 import (
 	"testing"
+	"time"
+
+	"github.com/go-redis/redis"
 )
 
 func Test_Authorization_CreateToken(t *testing.T) {
@@ -9,7 +12,7 @@ func Test_Authorization_CreateToken(t *testing.T) {
 	td, err := CreateToken("somekey")
 
 	if err != nil {
-		t.Error("Error when creatingg token details")
+		t.Error("Error when creating token details")
 	}
 
 	if len(td.AccessToken) == 0 {
@@ -20,4 +23,38 @@ func Test_Authorization_CreateToken(t *testing.T) {
 		t.Error("Must have refresh token")
 	}
 
+}
+
+func Test_Authorization_CreateAuth(t *testing.T) {
+
+	td := &TokenDetails{}
+
+	broker := new(MockBroker)
+
+	err := CreateAuth("somekey", td, broker)
+
+	if err != nil {
+		t.Error("Error when creating authentication")
+	}
+}
+
+type MockBroker struct {
+	//Publish(channel string, message interface{}) *redis.IntCmd
+	//Subscribe(channel string, cb func(string))
+	//Set(key string, message interface{}, time time.Duration) error
+}
+
+func (m *MockBroker) Subscribe(channel string, cb func(string)) {
+
+}
+
+func (m *MockBroker) Set(key string, message interface{}, time time.Duration) error {
+
+	return nil
+}
+
+func (m *MockBroker) Publish(channel string, message interface{}) *redis.IntCmd {
+
+	intCmd := &redis.IntCmd{}
+	return intCmd
 }
